1f0e31d78515aca70ec5ebf7e40a6714a9db28fd
zzdpk2 <zzdpk2@126.com>
2018-11-05 21:31:39 -0600

    new files

--- 85.java
+++ 85.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+
 public class MaximalRectangle {
 
     // DP:(right[]-left[])*height[]
@@ -8,6 +10,11 @@ public class MaximalRectangle {
     }
 
     public int MaximalRectangle(char[][] matrix) {
+
+        // left[],from left to right, the first coordinate of '1'
+        // right[],from right to left, the last coordinate of '1'
+        // height[], thickness
+        // res: (right[j] - left[j]) * height[j]
         int m = matrix.length;
 
         if (matrix == null || m == 0)
@@ -31,6 +38,25 @@ public class MaximalRectangle {
                 else
                     height[j] = 0;
             }
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == '1')
+                    left[j] = Math.max(curLeft, left[j]);
+                else {
+                    left[j] = 0;
+                    curleft = j + 1;
+                }
+                for (int j = n - 1; j >= 0; j--) {
+                    if (matrix[i][j] == '1')
+                        right[j] = Math.min(curRight, right[j])
+                    else{
+                        right[j] = n;
+                        curRight = j;
+                    }
+                }
+                for(int j = 0; j < n; j++)
+                    res = Math.max(res, ((right[j] - left[j]) * height[j])); 
+            }
+            return result;
         }
     }
 
