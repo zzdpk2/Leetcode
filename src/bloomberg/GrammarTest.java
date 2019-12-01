92b45a48c8940b1f948f3769c1ead74021f7828c
zzdpk2 <zzdpk2@126.com>
2019-11-30 21:56:51 -0600

    new

---
+++ src/bloomberg/GrammarTest.java
@@ -0,0 +1,15 @@
+package bloomberg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GrammarTest {
+    public static void main(String[] args) {
+
+        // Practice for Arrays.asList
+        String[] testArray = {"0", "1", "2", "3"};
+        List<String> strings = Arrays.asList(testArray);
+        for(String element: strings) System.out.println(element);
+    }
+}
---
+++ src/bloomberg/Sum3_15.java
@@ -0,0 +1,42 @@
+package bloomberg;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sum3_15 {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(nums);
+        for (int i = 0; i < result.size(); i++)     System.out.println(result.get(i));
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        if(nums[0] > 0 || nums[nums.length-1] < 0 || nums.length < 3)
+            return result;
+
+        int temp = 0, left = 0, right = 0;
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            temp = nums[i];
+            left = i + 1;
+            right = nums.length - 1;
+            while(left != right){
+                if(temp + nums[left] + nums[right] == 0){
+                    tempList.add(temp);
+                    tempList.add(nums[left]);
+                    tempList.add(nums[right]);
+                    result.add(tempList);
+                    tempList.clear();
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+}
