fe606eaff08162d1b57a2808ac2363583b0daa4b
zzdpk2 <zzdpk2@126.com>
2019-12-01 11:36:36 -0600

    new

--- src/bloomberg/Sum3_15.java
+++ src/bloomberg/Sum3_15.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Sum3_15 {
     public static void main(String[] args) {
-        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[] nums = {1,-1,-1,0};
         List<List<Integer>> result = threeSum(nums);
         for (int i = 0; i < result.size(); i++)     System.out.println(result.get(i));
 
@@ -15,25 +15,20 @@ public class Sum3_15 {
     public static List<List<Integer>> threeSum(int[] nums){
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        if(nums[0] > 0 || nums[nums.length - 1] < 0 || nums.length < 3)
+        if(nums.length < 3)
             return result;
 
         int temp = 0, left = 0, right = 0, val = 0;
-        ArrayList<Integer> tempList = new ArrayList<>();
         for (int i = 0; i < nums.length - 2; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             temp = nums[i];
             left = i + 1;
             right = nums.length - 1;
             val = -temp;
 
             while(left < right){
-                if(nums[left] + nums[right] == val){
-                    tempList.add(temp);
-                    tempList.add(nums[left]);
-                    tempList.add(nums[right]);
-                    result.add(tempList);
-                    tempList.clear();
-                }
+                if(nums[left] + nums[right] == val)
+                    result.add(Arrays.asList(temp, nums[left], nums[right]));
                 right--;
                 left++;
             }
